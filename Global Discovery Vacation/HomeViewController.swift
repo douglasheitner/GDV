024a45f8bb58a990d45d65ebf91185debd5c211f
Douglas Heitner <douglas.heitner@gmail.com>
2017-04-27 08:29:26 -0500

    Finished basic layout for the collection view.

--- Global Discovery Vacation/HomeViewController.swift
+++ Global Discovery Vacation/HomeViewController.swift
@@ -15,23 +15,25 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        collectionView?.backgroundColor = .white
+        collectionView?.backgroundColor = UIColor.lightGray
         collectionView?.register(ReservationCell.self, forCellWithReuseIdentifier: "cellId")
         navigationItem.title = "Reservations"
-        
+        collectionView?.contentInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 8
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-//        cell.backgroundColor = .green
+        cell.backgroundColor = .white
+        cell.layer.masksToBounds = true;
+        cell.layer.cornerRadius = 6;
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 100)
+        return CGSize(width: view.frame.width - 16, height: 120)
     }
 }
--- Global Discovery Vacation/ReservationCell.swift
+++ Global Discovery Vacation/ReservationCell.swift
@@ -17,16 +17,51 @@ class ReservationCell: UICollectionViewCell {
     
     let resortThumbnailImageView: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .yellow
+        imageView.image = #imageLiteral(resourceName: "resort-pic")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 2
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     let separatorView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black
+        view.backgroundColor = UIColor.lightGray
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    
+    let resortNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "SAND PEBBLES"
+        label.font = UIFont.systemFont(ofSize: 14, weight: UIFontWeightRegular)
+        return label
+    }()
+    
+    let checkInLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Check-In: Sat 12/9/2017"
+        label.font = UIFont.systemFont(ofSize: 12, weight: UIFontWeightThin)
+        return label
+    }()
+    
+    let checkOutLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Check-Out: Sat 12/16/2017"
+        label.font = UIFont.systemFont(ofSize: 12, weight: UIFontWeightThin)
+        return label
+    }()
+    
+    let reservationNumber: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Reservation #: 6949503"
+        label.font = UIFont.systemFont(ofSize: 12, weight: UIFontWeightLight)
+        return label
     }()
     
     func setupViews() {
@@ -39,9 +74,28 @@ class ReservationCell: UICollectionViewCell {
         addSubview(separatorView)
         separatorView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
         separatorView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
-        separatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 6).isActive = true
+        separatorView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 5.5).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
         
-        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        addSubview(resortNameLabel)
+        resortNameLabel.leftAnchor.constraint(equalTo: resortThumbnailImageView.rightAnchor, constant: 8).isActive = true
+        resortNameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        resortNameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -30).isActive = true
+        
+        addSubview(checkInLabel)
+        checkInLabel.leftAnchor.constraint(equalTo: resortThumbnailImageView.rightAnchor, constant: 8).isActive = true
+        checkInLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        checkInLabel.topAnchor.constraint(equalTo: resortNameLabel.bottomAnchor, constant: 4).isActive = true
+        
+        addSubview(checkOutLabel)
+        checkOutLabel.leftAnchor.constraint(equalTo: resortThumbnailImageView.rightAnchor, constant: 8).isActive = true
+        checkOutLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        checkOutLabel.topAnchor.constraint(equalTo: checkInLabel.bottomAnchor, constant: 4).isActive = true
+        
+        addSubview(reservationNumber)
+        reservationNumber.leftAnchor.constraint(equalTo: resortThumbnailImageView.rightAnchor, constant: 8).isActive = true
+        reservationNumber.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        reservationNumber.topAnchor.constraint(equalTo: checkOutLabel.bottomAnchor, constant: 4).isActive = true
 
     }
     
